cifsd-team/ksmbd-tools

commit 8ec5ca70e8bb87b2f2df7e9b7f40edb65a27a25b

ksmbd-tools: add max_len and actual_len arguments in ndr_write_vstring()
In other protocols, there is a difference between max length and acutal
length for ndr string. So add it to arguments so that both length are set
by caller.

Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

mountd/rpc_srvsvc.c
@@ -93,17 +93,22 @@ static int __share_entry_rep_ctr1(struct ksmbd_dcerpc *dce, gpointer entry)
 static int __share_entry_data_ctr0(struct ksmbd_dcerpc *dce, gpointer entry)
 {
 	struct ksmbd_share *share = entry;
+	size_t len;
 
-	return ndr_write_vstring(dce, share->name);
+	len = strlen(share->name) + 1;
+	return ndr_write_vstring(dce, share->name, len, len);
 }
 
 static int __share_entry_data_ctr1(struct ksmbd_dcerpc *dce, gpointer entry)
 {
 	struct ksmbd_share *share = entry;
 	int ret;
+	size_t len;
 
-	ret = ndr_write_vstring(dce, share->name);
-	ret |= ndr_write_vstring(dce, share->comment);
+	len = strlen(share->name) + 1;
+	ret = ndr_write_vstring(dce, share->name, len, len);
+	len = strlen(share->comment) + 1;
+	ret |= ndr_write_vstring(dce, share->comment, len, len);
 	return ret;
 }
 

include/rpc.h
@@ -299,7 +299,8 @@ __u32 ndr_read_union_int32(struct ksmbd_dcerpc *dce);
 
 int ndr_write_bytes(struct ksmbd_dcerpc *dce, void *value, size_t sz);
 int ndr_read_bytes(struct ksmbd_dcerpc *dce, void *value, size_t sz);
-int ndr_write_vstring(struct ksmbd_dcerpc *dce, char *value);
+int ndr_write_vstring(struct ksmbd_dcerpc *dce, void *value,
+		size_t max_len, size_t actual_len);
 char *ndr_read_vstring(struct ksmbd_dcerpc *dce);
 void ndr_read_vstring_ptr(struct ksmbd_dcerpc *dce, struct ndr_char_ptr *ctr);
 void ndr_read_uniq_vsting_ptr(struct ksmbd_dcerpc *dce,

mountd/rpc.c
@@ -382,20 +382,21 @@ int ndr_read_bytes(struct ksmbd_dcerpc *dce, void *value, size_t sz)
 	return 0;
 }
 
-int ndr_write_vstring(struct ksmbd_dcerpc *dce, char *value)
+int ndr_write_vstring(struct ksmbd_dcerpc *dce, void *value,
+		size_t max_len, size_t actual_len)
 {
 	gchar *out;
 	gsize bytes_read = 0;
 	gsize bytes_written = 0;
 
-	size_t raw_len;
 	char *raw_value = value;
 	int charset = KSMBD_CHARSET_UTF16LE;
 	int ret;
 
-	if (!value)
+	if (!value) {
 		raw_value = "";
-	raw_len = strlen(raw_value) + 1;
+		max_len = actual_len = strlen("") + 1;
+	}
 
 	if (!(dce->flags & KSMBD_DCERPC_LITTLE_ENDIAN))
 		charset = KSMBD_CHARSET_UTF16BE;
@@ -404,7 +405,7 @@ int ndr_write_vstring(struct ksmbd_dcerpc *dce, char *value)
 		charset = KSMBD_CHARSET_UTF8;
 
 	out = ksmbd_gconvert(raw_value,
-			     raw_len,
+			     actual_len,
 			     charset,
 			     KSMBD_CHARSET_DEFAULT,
 			     &bytes_read,
@@ -422,9 +423,9 @@ int ndr_write_vstring(struct ksmbd_dcerpc *dce, char *value)
 	 * The third integer gives the actual number of elements being
 	 * passed, including the terminator.
 	 */
-	ret = ndr_write_int32(dce, raw_len);
+	ret = ndr_write_int32(dce, max_len);
 	ret |= ndr_write_int32(dce, 0);
-	ret |= ndr_write_int32(dce, raw_len);
+	ret |= ndr_write_int32(dce, actual_len);
 	ret |= ndr_write_bytes(dce, out, bytes_written);
 	auto_align_offset(dce);
 

mountd/rpc_wkssvc.c
@@ -61,12 +61,15 @@ static int __netwksta_entry_data_ctr100(struct ksmbd_dcerpc *dce,
 					gpointer entry)
 {
 	int ret = 0;
+	size_t len;
 
 	/*
 	 * Umm... Hmm... Huh...
 	 */
-	ret |= ndr_write_vstring(dce, STR_VAL(dce->wi_req.server_name));
-	ret |= ndr_write_vstring(dce, global_conf.work_group);
+	len = strlen(STR_VAL(dce->wi_req.server_name)) + 1;
+	ret |= ndr_write_vstring(dce, STR_VAL(dce->wi_req.server_name), len, len);
+	len = strlen(global_conf.work_group) + 1;
+	ret |= ndr_write_vstring(dce, global_conf.work_group, len, len);
 	return ret;
 }
 

commit 04339277ba47cb0a0a82652ea2a8ba121a1d7e95

ksmbd-tools: fix potential null dereference of share->comment
share->comment can be null, depending on the config file. This is
already handled in line 61 in the same file but missing here.

Skip any operation on share->comment if it is null. This fixes a problem
when clients cannot connect to the ksmbd server anymore as ksmbd.mountd
fails with the following error in the kernel log:
  ksmbd: handle_startup_event:381: Reconnect to a new user space daemon
  do_page_fault(): sending SIGSEGV to pool for invalid read access from 00000000
  epc = 77ebaff4 in libc.so[77e42000+9c000]
  ra  = 0040dc3b in ksmbd.mountd[400000+15000]
  ksmbd: handle_startup_event:381: Reconnect to a new user space daemon

Fixes: 8ec5ca70e8bb87 ("ksmbd-tools: add max_len and actual_len arguments in ndr_write_vstring()")
Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

mountd/rpc_srvsvc.c
@@ -107,8 +107,12 @@ static int __share_entry_data_ctr1(struct ksmbd_dcerpc *dce, gpointer entry)
 
 	len = strlen(share->name) + 1;
 	ret = ndr_write_vstring(dce, share->name, len, len);
-	len = strlen(share->comment) + 1;
-	ret |= ndr_write_vstring(dce, share->comment, len, len);
+
+	if (share->comment) {
+		len = strlen(share->comment) + 1;
+		ret |= ndr_write_vstring(dce, share->comment, len, len);
+	}
+
 	return ret;
 }
 